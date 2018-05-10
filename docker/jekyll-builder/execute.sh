c9703ba4d909915a3aac2061cb246260aeb20b57
Mike Derksen <m.derksen@avisi.nl>
2018-05-10 10:10:16 +0200

    Added md-path-replace.sh to execute.sh

--- docker/jekyll-builder/execute.sh
+++ docker/jekyll-builder/execute.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-/usr/share/jekyll-builder/md-header.sh
 
+# Add missing headers to md files (needed for jekyll)
+/usr/share/jekyll-builder/add-md-headers.sh
+
+# Compile md files to html files
 bundle exec jekyll build \
         --config _config.yml \
         --source /usr/share/jekyll-builder/source \
         --destination /usr/share/jekyll-builder/output
+
+# Replace permalinks with .md extension to have a .html extension
+/usr/share/jekyll-builder/md-path-replace.sh
